0fa856db3c119dc28934d3e7c3e39bf67c0ea41f
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2022-12-09 23:25:13 -0800

    instances of arrays in c

---
+++ arrays.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // direct instanciation
+    int students_grages[8] = {3,
+                              5,
+                              7,
+                              8,
+                              9,
+                              30,
+                              45,
+                              68};
+
+    // indirect instanciation
+    int list[4];
+
+    list[0] = 45;
+    list[1] = 23;
+    list[2] = 12;
+    list[3] = 21;
+}
