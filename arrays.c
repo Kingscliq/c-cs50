9d9234a1d353b5109614dbf78417f1f0ed3f2ac0
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2022-12-13 14:46:11 -0800

    Loops in C

--- arrays.c
+++ arrays.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+void set_array(int arr[4]);
+void set_int(int x);
+
 int main(void)
 {
     // direct instanciation
@@ -19,4 +22,27 @@ int main(void)
     list[1] = 23;
     list[2] = 12;
     list[3] = 21;
+
+    int b[4] = {
+        0,
+        1,
+        2,
+        3,
+    };
+    int a = 23;
+    set_array(b);
+    set_int(a);
+    printf("%d, %d", a, b[0]);
+}
+
+// Declare a function to set Array
+void set_array(int arr[4])
+{
+    arr[0] = 22;
+}
+
+// Declare a function to set int
+void set_int(int x)
+{
+    x = 22;
 }
