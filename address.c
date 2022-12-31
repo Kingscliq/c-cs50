5bedc4e8961bfeb60d681c31e8ac8c2e72f2ecb4
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2022-12-30 22:48:01 -0800

    Pointer arithmetic for arrays

--- address.c
+++ address.c
@@ -8,8 +8,16 @@ int main(void)
     char *name = "string";
     char *s = "HI!";
 
+    int numbers[] = {2, 5, 7, 2, 9, 0}
+
+    // For Pointer arithmetic for strings of Characters
     printf("%c\n", *s);
     printf("%c\n", *(s + 1));
     printf("%c\n", *(s + 2));
     printf("%s\n", name);
+
+    // For Pointer Arithmetic for Arrays
+    printf("%i\n", *numbers);
+    printf("%i\n", *numbers + 1);
+    printf("%i\n", *numbers + 2);
 }
