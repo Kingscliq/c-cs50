c358dd562c4bd250a318d323b2558c8484cdb109
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-01 03:06:39 -0800

    using pointers instead of actual values

--- swap.c
+++ swap.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int swap(int a, int b);
+int swap(int *a, int *b);
 int main(void)
 {
     int x = 1;
     int y = 2;
 
     printf("x is %i and y is %i\n", x, y);
-    swap(x, y);
-    printf("x is %i and y is %i\n", x, y);
+    swap(&x, &y);
+    printf("x is %i and y is %i\n", x, y); // returns the same value as line 10 beacuse we didnt its making reference to the same value over and over again
 }
 
-int swap(int a, int b)
+// Intead of the values we use pointers
+int swap(int *a, int *b)
 {
+    int tmp = *a;
 
-    int tmp = a;
+    *a = *b;
 
-    a = b;
-
-    b = tmp;
+    *b = tmp;
 }
