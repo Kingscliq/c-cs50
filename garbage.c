c038ad7c10668777972d8865fc2b866693c852d1
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-01 02:15:13 -0800

    Error array not initialized

---
+++ garbage.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int scores[3];
+
+    // for (int i = 0; i < sizeof(scores); i++)
+    for (int i = 0; i < sizeof(scores); i++)
+    {
+        printf("%i\n", scores[i]);
+    }
+}
