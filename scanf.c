dc1998bd9c829723197ba20c2f51c02cd8296368
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-06 14:35:37 -0800

    Add: dynamic memory allocation

--- scanf.c
+++ scanf.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // A PROGRAM TO GET USER INPUT
 
 // clang -o scanf scanf.c (Command to run a prgram to get user input)
+
 int main(void)
 {
     int x;
 
-    printf("X: ");
-    scanf("%i", &x);
-    printf("x is %i\n", x);
+    // For Integers
+    // printf("X: ");
+    // scanf("%i", &x);
+    // printf("x is %i\n", x);
+
+    // For Strings
+    char *s = malloc(4); // malloc is used to assign memory locations to strings
+    printf("Value: ");
+    scanf("%s", s);
+    printf("Value is %s\n", s);
 }
