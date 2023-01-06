ca87ccaab6377d21c18fbeaf941911c6ddcf361c
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-06 15:01:38 -0800

    Allocating arrays like a hacker

--- list.c
+++ list.c
@@ -4,13 +4,18 @@
 int main(void)
 {
 
-    int *list = malloc(3 * sizeof(int));
+    int *list = malloc(3 * sizeof(int)); // Malloc is usualy used for dynamic memory allocation
     if (list == NULL)
     {
         return 1;
     }
 
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
+    // list[0] = 1;
+    // list[1] = 2;
+    // list[2] = 3;
+
+    // Another way of assyning arrays in a more dynamic way
+    *list = 1;
+    *(list + 1) = 2;
+    *(list + 2) = 3;
 }
