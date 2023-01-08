2005745fcac959842515ae3cd13274aba0d642ff
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-07 23:27:14 -0800

    comment

--- linked.c
+++ linked.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
+typedef struct node // if the typedef is removed then u will likely be using struct node throughout the app
+{
+    int number;
+    struct node *next;
+
+} node; // typedef helps u create your own custm type giving it a unique name
+
 int main(void)
 {
-
-    typedef struct node
-    {
-        int number;
-        struct node *next;
-
-    } node;
 }
