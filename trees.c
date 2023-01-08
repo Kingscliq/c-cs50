b96c24839e869fe31a7e02b5724369190c961bf9
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-08 06:37:27 -0800

    Add: defining a structure for out tree

--- trees.c
+++ trees.c
@@ -9,4 +9,19 @@ typedef struct node
 
 int main(void)
 {
+    node *list;
+
+    int *n = malloc(sizeof(node));
+
+    if (n == NULL)
+    {
+        return 1;
+    }
+
+    n->number = 2;
+    n->left = NULL;
+    n->right = NULL;
+
+    // Update the value of list to be n
+    list = n;
 }
